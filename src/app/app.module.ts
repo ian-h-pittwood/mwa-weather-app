e57617f9604e7f7e0b5e01eb1ebb1815bfcaa598
Diana Reyes <Diana.Reyes@accenture.com>
2019-03-19 14:45:43 -0700

    added city-thumbnail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,18 @@ import { AppComponent } from './app.component';
 import { CityListComponent } from './city-list/city-list.component';
 import { CityDetailComponent } from './city-detail/city-detail.component'
 import { NavComponent } from './nav/nav.component';
+import { CityThumbnailComponent } from './city-thumbnail/city-thumbnail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CityListComponent,
     NavComponent,
+<<<<<<< HEAD
     CityDetailComponent,
+=======
+    CityThumbnailComponent
+>>>>>>> 203a3da... added city-thumbnail component
   ],
   imports: [
     BrowserModule
---
+++ src/app/city-thumbnail/city-thumbnail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-city-thumbnail',
+  templateUrl: './city-thumbnail.component.html',
+  styleUrls: ['./city-thumbnail.component.sass']
+})
+export class CityThumbnailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+ 
