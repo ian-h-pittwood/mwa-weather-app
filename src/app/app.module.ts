c57de351b61e663fd370cb4090c7b789ee9fe7b4
Ian Pittwood <ihpyr4@mst.edu>
2019-03-18 19:39:39 -0500

    [#2] Finished City List component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CityListComponent } from './city-list/city-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CityListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/city-list/city-list.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-city-list',
+  templateUrl: './city-list.component.html',
+  styleUrls: ['./city-list.component.sass']
+})
+export class CityListComponent implements OnInit {
+  cityData = {
+    list: [
+      {
+        coord: {lon: -0.13, lat: 51.51},
+        weather: [{id: 300, main: 'Drizzle', description: 'light intensity drizzle', icon: '09d'}],
+        base: 'stations',
+        main: {temp: 7.17, pressure: 1012, humidity: 81, temp_min: 6, temp_max: 8},
+        visibility: 10000,
+        wind: {speed: 4.1, deg: 80},
+        clouds: {all: 90},
+        dt: 1485789600,
+        sys: {type: 1, id: 5091, message: 0.0103, country: 'GB', sunrise: 1485762037, sunset: 1485794875},
+        id: 2643743,
+        name: 'London',
+        cod: 200
+      }
+    ]
+  };
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
