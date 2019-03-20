0a2820ac0955a02d9a36b3651d136bfd2a0e6eae
andrewrfowler <andrewrfowler111@gmail.com>
2019-03-20 15:11:43 -0400

    Added service for city list

--- src/app/city-list/city-list.component.ts
+++ src/app/city-list/city-list.component.ts
@@ -1,93 +1,25 @@
 import {Component, OnInit} from '@angular/core';
+import { CityListService } from './Shared/city-list-service';
 
 @Component({
   selector: 'app-city-list',
   templateUrl: './city-list.component.html',
   styleUrls: ['./city-list.component.sass']
 })
+
 export class CityListComponent implements OnInit {
   // TODO: This is a placeholder for the 5 city hard coded data (later on it will be replaced by the service)
-  cityDataList = {
-    list: [
-      {
-        coord: {lon: -0.13, lat: 51.51},
-        weather: [{id: 300, main: 'Drizzle', description: 'light intensity drizzle', icon: '09d'}],
-        base: 'stations',
-        main: {temp: 7.2, pressure: 1012, humidity: 81, temp_min: 6, temp_max: 8},
-        visibility: 10000,
-        wind: {speed: 4.1, deg: 80},
-        clouds: {all: 90},
-        dt: 1485789600,
-        sys: {type: 1, id: 5091, message: 0.0103, country: 'GB', sunrise: 1485762037, sunset: 1485794875},
-        id: 2643743,
-        name: 'London',
-        cod: 200,
-      },
-      {
-        coord:{lon:145.77,lat:-16.92},
-        weather:[{id:803,main: 'Clouds' , description:'broken clouds',icon:'04n'}],
-        base:'cmc stations',
-        main: {temp: 20.1, pressure: 1019, humidity: 83, temp_min: 16, temp_max: 22.2},
-        wind:{speed:5.1,deg:150},
-        clouds:{all:75},
-        dt:1435658272,
-        sys:{type:1,id:8166,message:0.0166,country:'AU',sunrise:1435610796,sunset:1435650870},
-        id:2172797,
-        name:"Cairns",
-        cod:200,
-      },
+  citynames : any []
+  constructor ( private cityListService: CityListService){
 
-      {
-        coord:{lon:145.77,lat:-16.92},
-        weather:[{id:803,main: 'Rain' , description:'Light Rain',icon:'04n'}],
-        base:'cmc stations',
-        main: {temp: 9, pressure: 1019, humidity: 83, temp_min: 8.7, temp_max: 12.3},
-        wind:{speed:5.1,deg:150},
-        clouds:{all:75},
-        dt:1435658272,
-        sys:{type:1,id:8166,message:0.0166,country:'AU',sunrise:1435610796,sunset:1435650870},
-        id:2172797,
-        name: "New York",
-        cod:200,
-      },
-
-      {
-        coord:{lon:145.77,lat:-16.92},
-        weather:[{id:803,main: 'Snow' , description:'Heavy Snow',icon:'04n'}],
-        base:'cmc stations',
-        main: {temp: -5.9, pressure: 1019, humidity: 83, temp_min: -8.1, temp_max: -4.2},
-        wind:{speed:5.1,deg:150},
-        clouds:{all:75},
-        dt:1435658272,
-        sys:{type:1,id:8166,message:0.0166,country:'AU',sunrise:1435610796,sunset:1435650870},
-        id:2172797,
-        name:"Syracuse",
-        cod:200,
-      },
-
-      {
-        coord:{lon:145.77,lat:-16.92},
-        weather:[{id:803,main: 'Sunny' , description:'Sunny Skies',icon:'04n'}],
-        base:'cmc stations',
-        main: {temp: 12, pressure: 1019, humidity: 83, temp_min: 6.7, temp_max: 12.2},
-        wind:{speed:5.1,deg:150},
-        clouds:{all:75},
-        dt:1435658272,
-        sys:{type:1,id:8166,message:0.0166,country:'AU',sunrise:1435610796,sunset:1435650870},
-        id:2172797,
-        name:"Los Angeles",
-        cod:200,
-      },
-
-      
-      
-    ]
-  };
-
-  constructor() {
   }
 
-  ngOnInit() {
+  ngOnInit(){
+    console.log("Return from service", this.cityListService.getCityData())
+
+    this.citynames = this.cityListService.getCityData().list
+
+
   }
 
 }
