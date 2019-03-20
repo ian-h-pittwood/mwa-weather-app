ae6f467bcfe4d7eb8b29205b330debba1d9e1b8d
andrewrfowler <andrewrfowler111@gmail.com>
2019-03-20 10:58:21 -0400

    Hard coded 5 City data

--- src/app/city-list/city-list.component.ts
+++ src/app/city-list/city-list.component.ts
@@ -21,8 +21,66 @@ export class CityListComponent implements OnInit {
         sys: {type: 1, id: 5091, message: 0.0103, country: 'GB', sunrise: 1485762037, sunset: 1485794875},
         id: 2643743,
         name: 'London',
-        cod: 200
-      }
+        cod: 200,
+      },
+      {
+        coord:{lon:145.77,lat:-16.92},
+        weather:[{id:803,main: 'Clouds' , description:'broken clouds',icon:'04n'}],
+        base:'cmc stations',
+        main:{temp:293.25,pressure:1019,humidity:83,temp_min:289.82,temp_max:295.37},
+        wind:{speed:5.1,deg:150},
+        clouds:{all:75},
+        dt:1435658272,
+        sys:{type:1,id:8166,message:0.0166,country:'AU',sunrise:1435610796,sunset:1435650870},
+        id:2172797,
+        name:"Cairns",
+        cod:200,
+      },
+
+      {
+        coord:{lon:145.77,lat:-16.92},
+        weather:[{id:803,main: 'Rain' , description:'Light Rain',icon:'04n'}],
+        base:'cmc stations',
+        main:{temp:9,pressure:1019,humidity:83,temp_min:289.82,temp_max:295.37},
+        wind:{speed:5.1,deg:150},
+        clouds:{all:75},
+        dt:1435658272,
+        sys:{type:1,id:8166,message:0.0166,country:'AU',sunrise:1435610796,sunset:1435650870},
+        id:2172797,
+        name: "New York",
+        cod:200,
+      },
+
+      {
+        coord:{lon:145.77,lat:-16.92},
+        weather:[{id:803,main: 'Snow' , description:'Heavy Snow',icon:'04n'}],
+        base:'cmc stations',
+        main:{temp:-5.9,pressure:1019,humidity:83,temp_min:289.82,temp_max:295.37},
+        wind:{speed:5.1,deg:150},
+        clouds:{all:75},
+        dt:1435658272,
+        sys:{type:1,id:8166,message:0.0166,country:'AU',sunrise:1435610796,sunset:1435650870},
+        id:2172797,
+        name:"Syracuse",
+        cod:200,
+      },
+
+      {
+        coord:{lon:145.77,lat:-16.92},
+        weather:[{id:803,main: 'Sunny' , description:'Sunny Skies',icon:'04n'}],
+        base:'cmc stations',
+        main:{temp:12,pressure:1019,humidity:83,temp_min:289.82,temp_max:295.37},
+        wind:{speed:5.1,deg:150},
+        clouds:{all:75},
+        dt:1435658272,
+        sys:{type:1,id:8166,message:0.0166,country:'AU',sunrise:1435610796,sunset:1435650870},
+        id:2172797,
+        name:"Los Angeles",
+        cod:200,
+      },
+
+      
+      
     ]
   };
 
